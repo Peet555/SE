760ec22d0384312796bb4abfb731a511b827443a
annchana <annchaoat@gmail.com>
2024-09-03 12:38:14 +0700

    Kafe Web

---
+++ kafe/src/main/java/ku/ac/kafe/KafeWebApplication.java
@@ -0,0 +1,13 @@
+package ku.ac.kafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafeWebApplication.class, args);
+	}
+
+}
